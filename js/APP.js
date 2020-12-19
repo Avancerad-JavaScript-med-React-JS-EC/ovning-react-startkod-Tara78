cd7618b527c4221c68e0a139226f9811d6923dfe
Tara78 <70331037+Tara78@users.noreply.github.com>
2020-12-19 11:22:50 +0100

    react

---
+++ js/APP.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Footer from './Footer'
+
+function APP(){
+    return(
+        <div>
+            <h1>Hello Wolrd</h1>
+            <p>This is a paraghearph </p>
+          <nav>
+          <ul>
+                <li>Nr: 1</li>
+                <li> Nr:2</li>
+
+            </ul>
+          </nav>
+<main>
+    <p>
+        this is the second part
+    </p>
+</main>
+<Footer />
+        </div>
+    )
+}
+
+export default APP 
